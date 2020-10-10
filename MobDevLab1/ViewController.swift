bcd3e9419c0380c358132300cdc459d5eeb6f976
Yustina Ovcharova <jusovch2000@gmail.com>
2020-10-10 23:51:02 +0300

    Initial Commit

---
+++ MobDevLab1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobDevLab1
+//
+//  Created by Юстина Овчарова on 10.10.2020.
+//  Copyright © 2020 Yustina Ovcharova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
