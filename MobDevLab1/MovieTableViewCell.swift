8e9b72c7f1923544de45344f9d632ff037b54803
Yustina Ovcharova <jusovch2000@gmail.com>
2020-11-22 22:59:57 +0200

    Updated by adding Lab3's task

---
+++ MobDevLab1/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  MobDevLab1
+//
+//  Created by Юстина Овчарова on 08.11.2020.
+//  Copyright © 2020 Yustina Ovcharova. All rights reserved.
+//
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
